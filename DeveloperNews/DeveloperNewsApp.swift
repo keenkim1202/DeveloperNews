0114e76519240f42fb89842870bf3be34f5d9d22
KEEN <59866819+keenkim1202@users.noreply.github.com>
2026-04-12 01:35:53 +0900

    Increase shared cache budget for feed images (#30)

--- DeveloperNews/DeveloperNewsApp.swift
+++ DeveloperNews/DeveloperNewsApp.swift
@@ -5,13 +5,24 @@
 //  Created by KEEN on 4/11/26.
 //
 
+import Foundation
 import SwiftUI
 
 @main
 struct DeveloperNewsApp: App {
+    init() {
+        configureSharedImageCache()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+
+    private func configureSharedImageCache() {
+        let memoryCapacity = 50 * 1024 * 1024
+        let diskCapacity = 200 * 1024 * 1024
+        URLCache.shared = URLCache(memoryCapacity: memoryCapacity, diskCapacity: diskCapacity, diskPath: "imageCache")
     }
 }
