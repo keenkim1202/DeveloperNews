0954616d263a035747d6b03ea65e29185b5a4b7e
keen <homurahomus@gmail.com>
2026-04-11 22:18:56 +0900

    Add initial iOS app project

---
+++ DeveloperNews/DeveloperNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperNewsApp.swift
+//  DeveloperNews
+//
+//  Created by KEEN on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DeveloperNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
